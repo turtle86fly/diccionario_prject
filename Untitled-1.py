325aa80fcf6900abc8e1463fc1754b1e4067276e
ANY VAZQUEZ <jonmerher1986@gmail.com>
2026-07-23 18:16:05 -0600

    actualizado directorio

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,4 +4,9 @@ dic={"marca":"sony",
 
 palabra=input("ingresa palabra : ")
 
-print(dic[palabra])
+print(dic[palabra],"\n")
+
+list=["mexico","canada","colombia","peru","chile","argentina","uruguay","costa rica"]
+
+for r in list:
+    print(r)
